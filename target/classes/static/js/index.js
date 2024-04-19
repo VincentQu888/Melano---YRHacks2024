b44a13635a3cd2536ba8cdb314ffd1381716ea18
VincentQu888 <112832158+VincentQu888@users.noreply.github.com>
2024-04-18 20:59:43 -0400

    Delete target directory

--- target/classes/static/js/index.js
+++
@@ -1,9 +0,0 @@
-const left = document.getElementById("left");
-
-const handleOnMove = e => {
-    const p = e.clientX / window.innerWidth*100;
-
-    left.style.width = `${p}%`;
-}
-
-document.onmousemove = e => handleOnMove(e);
